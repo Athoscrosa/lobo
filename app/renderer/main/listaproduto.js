a12767da73950280d1a8d8680235e3636653baf2
Athos Cordeiro Rosa <athoscordeiro@hotmail.com>
2026-03-17 22:33:11 -0300

    c
    c

--- app/renderer/main/listaproduto.js
+++ app/renderer/main/listaproduto.js
@@ -1,5 +1,31 @@
-let table = new DataTable("#tabela", {
+const { Column } = require("drizzle-orm");
+
+const table = new DataTable("#tabela-produto", {
     responsive: true,
+    processing: true,
+    serverSide: true,
+    ajax: async(data, callback) => {
+        const filter = {
+            term: data?.search?.value,             //termo da pesquisa
+            limit: data?.length,                 //Limite de registros a ser selecionado do banco
+            offset: data?.start,                   // A pesquisa inicia no registro Ex: 5, 10
+            orderType: data?.order[0]?.dir,              // tipo de ordenação (asc ou desc)
+            column: data?.order[0]?.column         //coluna a ser filtrada
+        }
+        const response = await window.electronAPI.searchProducts(filter);
+        callback({
+            draw: response?.draw ?? data?.draw ?? 0,                     //contador de requisições
+            recordsTotal: response?.recordsTotal ?? 0,                     //total de registros no banco
+            recordsFiltered: response?.recordsFiltered ?? 0,             //total de registros filtrados
+            data: response?.data ?? []                                       //dados a serem exibidos na tabela
+        });
+       
+    },
+    columns: [
+        { data: "codigo", name: "codigo" },
+        { data: "nome", name: "nome" },
+        { data: "price", name: "Preço de Venda" }
+    ]
 });
 const voltarButton = document.getElementById('voltar-button');
 const cadastroButton = document.getElementById('cadastro-button');
